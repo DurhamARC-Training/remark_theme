72501be617075c14afd137123181abc6cdcf917d
Dmitry Nikolaenko <dmitry.nikolaenko@durham.ac.uk>
2024-04-16 13:00:15 +0100

    Using small durham uni logo

--- durham.js
+++ durham.js
@@ -29,7 +29,7 @@ function DurhamTheme() {
     // Capture this for use in closures
     var self = this;
     this.header = function() {
-	return self.subst("<img src=\"$BASEURL/durham-violet.png\" />");
+	return self.subst("<img src=\"$BASEURL/durham-small-violet.png\" />");
     };
     this.footer_text = "";
     this.footer = function() {
